3e793665661ccd1cca33a60272c2234a58ac048e
LNgroff <LNgroff@gmail.com>
2020-10-08 21:04:08 +0000

    Harvest Report Complete
    Co-authored-by: georgiaschmitt <georgia.schmitt@gmail.com>

--- harvest.py
+++ harvest.py
@@ -86,13 +86,40 @@ def make_melon_type_lookup(melon_types):
 class Melon(object):
     """A melon in a melon harvest."""
 
-    # Fill in the rest
-    # Needs __init__ and is_sellable methods
+    def __init__(self, melon_type, shape_rating, color_rating,
+                field, harvester):
+
+        self.melon_type = melon_type
+        self.shape_rating = shape_rating
+        self.color_rating = color_rating
+        self.field = field
+        self.harvester = harvester
+    
+    def is_sellable():
+        return self.shape_rating > 5 and self.color_rating > 5 and self.field != 3
+
 
 def make_melons(melon_types):
     """Returns a list of Melon objects."""
 
-    # Fill in the rest
+    melons = []
+
+    melons_dict = make_melon_type_lookup(melon_types)
+
+    melon_1 = Melon(melons_dict['yw'], 8, 7, 2, 'Sheila')
+    melon_2 = Melon(melons_dict['yw'], 3, 4, 2, 'Sheila')
+    melon_3 = Melon(melons_dict['yw'], 9, 8, 3, 'Sheila')
+    melon_4 = Melon(melons_dict['cas'], 10, 6, 35, 'Sheila')
+    melon_5 = Melon(melons_dict['cren'], 8, 9, 35, 'Michael')
+    melon_6 = Melon(melons_dict['cren'], 8, 2, 35, 'Michael')
+    melon_7 = Melon(melons_dict['cren'], 2, 3, 4, 'Michael')
+    melon_8 = Melon(melons_dict['musk'], 6, 7, 4, 'Michael')
+    melon_9 = Melon(melons_dict['yw'], 7, 10, 3, 'Sheila')
+
+    melons.extend([melon_1, melon_2, melon_3, melon_4, melon_5,
+                melon_6, melon_7, melon_8, melon_9])
+
+    return melons
 
 def get_sellability_report(melons):
     """Given a list of melon object, prints whether each one is sellable."""
